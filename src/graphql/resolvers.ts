d8a728ad16721415f52f4e64c19031ab7118babd
walidChaybi <98161047+walidChaybi@users.noreply.github.com>
2023-10-26 11:15:21 +0200

    connected Db with Apollo, added delete resolver to delete from the database

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,21 +1,27 @@
-import { listings } from "../listings";
+import { Database } from "../lib/types";
 import { IResolvers } from "apollo-server-express";
+import { ObjectId } from "mongodb";
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
-      return listings;
+    listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+      return await db.listings.find({}).toArray();
     },
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < listings.length; i++) {
-        if (listings[i].id === id) {
-          return listings.splice(i, 1)[0];
-        }
-      }
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ) => {
+      const deleteRes = await db.listings.findOneAndDelete({
+        _id: new ObjectId(id),
+      });
 
-      throw new Error("failed to deleted listing");
+      if (!deleteRes) {
+        throw new Error("failed to delete listing");
+      }
+      return deleteRes;
     },
   },
 };
